7a16c6f7db2785f187fde6bc1a01309c4e835e10
Josue-Molina1 <Josue11183@byui.edu>
2023-04-03 13:06:33 -0600

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+let a = 10
+if a < 4{
+    print("only if is less than 4")
+}
+else if a < 9{
+    print("only if is greater than 5")
+}
+else {
+    print("nothing was true :(")
+    }
+
+var josue:Int
+
+josue = 10
+print(josue)
